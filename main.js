46d240daa6f596479c5ce11f0293448d24ba0da8
Ahmed Khaled <ahmed.khaledko17@gmail.com>
2024-12-07 14:52:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function detailCard(arr) {
      details +=`<div>
 <div class="mx-24 my-10 text-white text-[32px] font-bold flex justify-between">
     <h1>Game Details</h1>
-    <a class="text-[36px] btn"><i class="fa-regular fa-circle-xmark" style="color: #ffffff;"></i></a>
+    <a class="text-[36px] btn"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg></a>
 </div>
     <div class="details md:flex text-white mx-24 gap-8">
         <div class="w-full md:h-60 md:w-1/3">
